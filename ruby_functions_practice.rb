a0f21bfa7f2a8a4ce0bae619f48e80c98061629b
Gerry Waterston <gwaterston@hotmail.co.uk>
2018-11-20 18:58:57 +0000

    test_join_string complete

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -22,3 +22,9 @@ def length_of_string(string)
   string = "A string of length 21"
   return string.length
 end
+
+def join_string(string_1, string_2)
+  string_1 = "Mary had a little lamb, "
+  string_2 = "its fleece was white as snow"
+  return string_1.concat(string_2)
+end
