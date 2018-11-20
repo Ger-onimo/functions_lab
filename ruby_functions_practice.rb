e4159cefb4aaa68fe234c5a272703dfdfd0f9140
Gerry Waterston <gwaterston@hotmail.co.uk>
2018-11-20 19:35:51 +0000

    test_length_of_string redone

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -19,12 +19,16 @@ def divide(num_1, num_2)
 end
 
 def length_of_string(string)
-  string = "A string of length 21"
   return string.length
 end
 
-def join_string(string_1, string_2)
-  string_1 = "Mary had a little lamb, "
-  string_2 = "its fleece was white as snow"
-  return string_1.concat(string_2)
-end
+# def join_string(string_1, string_2)
+#   string_1 = "Mary had a little lamb, "
+#   string_2 = "its fleece was white as snow"
+#   return string_1.concat(string_2)
+# end
+#
+# def add_string_as_number(string_1, string_2)
+#   string_1. =
+#   return .to_i
+# end
