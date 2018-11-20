425c5c711f47395c73d9a81147ff5a40409244ef
Gerry Waterston <gwaterston@hotmail.co.uk>
2018-11-20 19:42:55 +0000

    test_add_string_as_number complete

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -26,7 +26,6 @@ def join_string(string_1, string_2)
   return string_1.concat(string_2)
 end
 
-# def add_string_as_number(string_1, string_2)
-#   string_1. =
-#   return .to_i
-# end
+def add_string_as_number(string_1, string_2)
+  return string_1.to_i + string_2.to_i
+end
